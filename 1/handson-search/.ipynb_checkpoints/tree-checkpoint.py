ad970845239d118e904f5467895d9662b744cbd5
giovanniGuerrazzi <giovanniguerrazzi99g@gmail.com>
2023-12-17 12:08:43 +0100

    eliminati file non utili

--- 1/handson-search/.ipynb_checkpoints/tree-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def stampa_albero(n):
-    for i in range(1, n + 1):
-        spazi = " " * (n - i)
-        asterischi = "*" * (2 * i - 1)
-        print(spazi + asterischi)
-
-n = int(input("Inserisci l'altezza dell'albero di Natale: "))
-stampa_albero(n)
